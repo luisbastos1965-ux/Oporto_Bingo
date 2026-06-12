38be6ec38b1999d828bae165e59c19324b19ee3a
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-06-12 08:51:17 +0100

    Add basic service worker with install and fetch events

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+self.addEventListener('fetch', (e) => {
+  // Configuração básica para permitir a instalação
+});

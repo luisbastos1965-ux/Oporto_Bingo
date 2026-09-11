c75a8e7e9a29059983dd1d847dc32d2d69cf64d4
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-09-11 21:38:56 +0100

    .

--- app.js
+++ app.js
@@ -586,7 +586,7 @@ function initGPS() {
 
 document.addEventListener("visibilitychange", () => {
     if (document.visibilityState === "visible") {
-        if (localStorage.getItem('oportoBingoIntroSeen_25') === 'true') {
+        if (localStorage.getItem('oportoBingoIntroSeen_26') === 'true') {
             initGPS();
         }
     }

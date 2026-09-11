0bec8474fa2ae667c73c8520df98f22ee49003f9
luisbastos1965-ux <luisbastos1965@gmail.com>
2026-09-11 20:36:57 +0100

    .

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // O nome da tua cache. Quando fizeres grandes alterações no design no futuro, 
-// muda isto para 'oporto-bingo-v19' para forçar os telemóveis a atualizarem.
-const CACHE_NAME = 'oporto-bingo-v19';
+// muda isto para 'oporto-bingo-v21' para forçar os telemóveis a atualizarem.
+const CACHE_NAME = 'oporto-bingo-v21';
 
 // 1. INSTALAÇÃO: Força a atualização imediata do Service Worker
 self.addEventListener('install', (event) => {
